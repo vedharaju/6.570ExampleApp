d2eec6fabd24d74dadde0e2728384a73fed927a2
saylorp <psaylor@mit.edu>
2012-12-25 23:07:12 -0600

    Start button now takes you to the gallery to select a picture and we get the picture filepath in onActivityResult. Tested in AVD- good.

--- src/com/vedha/sampleapp/HomeActivity.java
+++ src/com/vedha/sampleapp/HomeActivity.java
@@ -1,15 +1,36 @@
 package com.vedha.sampleapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
+
+	public static final int RESULT_IMAGE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		Button b = (Button) findViewById(R.id.start_button);
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(
+						Intent.ACTION_PICK,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, RESULT_IMAGE);
+			}
+		});
 	}
 
 	@Override
@@ -19,4 +40,24 @@ public class HomeActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == RESULT_IMAGE && resultCode == RESULT_OK
+				&& null != data) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+			Cursor cursor = getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+			Toast.makeText(this, "picture selected!", Toast.LENGTH_SHORT).show();
+			// String picturePath contains the path of selected Image
+		}
+	}
 }
