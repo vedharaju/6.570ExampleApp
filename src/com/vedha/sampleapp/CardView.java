a760501e36522123cf99e053347fd2d3e808c064
saylorp <psaylor@mit.edu>
2013-01-07 22:03:37 -0500

    Added a send button the cardmaker layout and an onclick listener for it that sends an implicit intent to send a message with an attached image (which it gets from a saved Uri)

---
+++ src/com/vedha/sampleapp/CardView.java
@@ -0,0 +1,20 @@
+package com.vedha.sampleapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class CardView extends View {
+
+	public CardView(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+	
+	
+
+}
